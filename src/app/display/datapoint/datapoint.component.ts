4232e622c83a3440e3d8037089f6788daf0b6387
Wes Copeland <wes.copeland@overwatchid.com>
2019-05-14 14:02:34 -0400

    feat(display): show all datapoints with real data

--- src/app/display/datapoint/datapoint.component.ts
+++ src/app/display/datapoint/datapoint.component.ts
@@ -1,13 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import { mean, sum } from 'lodash';
 
 @Component({
   selector: 'ssl-datapoint',
   templateUrl: './datapoint.component.html',
   styleUrls: ['./datapoint.component.scss']
 })
-export class DatapointComponent implements OnInit {
+export class DatapointComponent implements OnInit, OnChanges {
+  public renderAverage: number;
+  public renderMostRecent: number;
+  public renderSum: number;
+
   @Input() label: string;
   @Input() averageOfNumbers: number[];
+  @Input() mostRecentOfNumbers: number[];
   @Input() sumOfNumbers: number[];
   @Input() directNumber: number;
   @Input() directString: string;
@@ -15,4 +21,20 @@ export class DatapointComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  ngOnChanges(e) {
+    if (this.averageOfNumbers) {
+      this.renderAverage = mean(this.averageOfNumbers);
+    }
+
+    if (this.mostRecentOfNumbers) {
+      this.renderMostRecent = this.mostRecentOfNumbers[
+        this.mostRecentOfNumbers.length - 1
+      ];
+    }
+
+    if (this.sumOfNumbers) {
+      this.renderSum = sum(this.sumOfNumbers);
+    }
+  }
 }
