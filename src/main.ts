2d1fa8b9faa5078fdb97d9bb7d359a03d8ddf464
Wes Copeland <wes.copeland@overwatchid.com>
2019-05-15 15:03:43 -0400

    feat(state): persist settings between app usage

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import 'hammerjs';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableAkitaProdMode, persistState } from '@datorama/akita';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+
+  enableAkitaProdMode();
+  persistState({
+    include: ['settings']
+  });
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch(err => console.error(err));
